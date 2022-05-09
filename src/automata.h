9bf64e9b4fe3f158c2a04d54e575074f47570edb
apaz-cli <aarpazdera@gmail.com>
2022-05-09 16:15:31 -0500

    Using Brzozowski's minimization algorithm.

--- src/automata.h
+++ src/automata.h
@@ -1,5 +1,32 @@
 #ifndef PGEN_AUTOMATA_INCLUDE
 #define PGEN_AUTOMATA_INCLUDE
+#include "util.h"
+#include "utf8.h"
+#include "list.h"
+#include "ast.h"
+
+struct DFANode;
+typedef struct DFANode DFANode;
+
+typedef struct {
+  ASTNode* charset;
+  int from;
+  int to;
+} DFATransition;
+LIST_DECLARE(DFATransition);
+LIST_DEFINE(DFATransition);
+
+LIST_DECLARE(int);
+LIST_DEFINE(int);
+
+typedef struct {
+  list_int accepting_states;
+  list_DFATransition transitions;
+} NFA;
+
+typedef struct {
+
+} DFA
 
 
 #endif /* PGEN_AUTOMATA_INCLUDE */
