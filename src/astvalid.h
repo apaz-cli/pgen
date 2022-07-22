357ebdb59f667160ca0c1667c6a0548814034ea5
apaz-cli <aarpazdera@gmail.com>
2022-07-22 11:53:10 -0500

    Added validation to rules, tokens, and labels.

--- src/astvalid.h
+++ src/astvalid.h
@@ -6,8 +6,6 @@
 // TODO: Make sure that every SM transition from has
 //       an SM transition to unless it's zero.
 
-// TODO: Make sure that every upperident in the peg corresponds to a token
-// TODO: Make sure that every lowerident in the peg corresponds to a rule
 // TODO: Make sure that no names of labels and rules overlap
 // TODO: Make sure that no labels are named rule or ret.
 // TODO: Make sure that labeled ModExprs do not have ModExprLists in them with
@@ -101,60 +99,94 @@ static inline void resolveReplace(ASTNode *node, char *prev_name,
 typedef ASTNode *ASTNodePtr;
 LIST_DECLARE(ASTNodePtr)
 LIST_DEFINE(ASTNodePtr)
-static inline void resolvePrevNext(ASTNode *pegast) {
+typedef char *charptr;
+LIST_DECLARE(charptr)
+LIST_DEFINE(charptr)
 
-  // Filter for definitions
+// Also pulls out all the names of the rules.
+static inline list_charptr resolvePrevNext(ASTNode *pegast) {
+
+  // Filter for definitions, pull out names
   list_ASTNodePtr defs = list_ASTNodePtr_new();
+  list_charptr names = list_charptr_new();
   for (size_t i = 0; i < pegast->num_children; i++) {
     ASTNode *def = pegast->children[i];
     if (strcmp(def->name, "Definition"))
       continue;
     list_ASTNodePtr_add(&defs, def);
+    list_charptr_add(&names, (char *)def->children[0]->extra);
   }
 
   // Resolve prev and next by replacing content of LowerIdents.
   char *prev_name = NULL, *next_name = NULL;
-  for (size_t i = 0; i < defs.len; i++) {
-    prev_name = i ? (char *)defs.buf[i - 1]->children[0]->extra : NULL;
-    next_name = (i == defs.len - 1)
-                    ? NULL
-                    : (char *)defs.buf[i + 1]->children[0]->extra;
+  for (size_t i = 0; i < names.len; i++) {
+    prev_name = i ? names.buf[i - 1] : NULL;
+    next_name = (i == defs.len - 1) ? NULL : names.buf[i + 1];
     resolveReplace(defs.buf[i], prev_name, next_name);
   }
 
   list_ASTNodePtr_clear(&defs);
+  return names;
 }
 
-static inline int validateIsTokName(ASTNode *tokast, char *name) {
-
-  for (size_t i = 0; i < tokast->num_children; i++) {
-    ASTNode *tokendef = tokast->children[i];
-    ASTNode *tokident = tokendef->children[0];
-    char *tokname = (char *)tokident->extra;
-    if (!strcmp(name, tokname))
-      return 1;
+static inline void validateVisitLabel(ASTNode *label, list_charptr *names) {
+  char *lname = (char *)label->extra;
+  for (size_t i = 0; i < names->len; i++) {
+    if (!strcmp(lname, names->buf[i]))
+      ERROR("Cannot use %s as both a rule and a label.", lname);
   }
-  return 0;
 }
 
-static inline void validatePegVisit(ASTNode *node, ASTNode *tokast) {
+static inline void validatePegVisit(ASTNode *node, ASTNode *tokast,
+                                    list_charptr *names) {
+  // Pull out all the upperidents from pegast and find them in the tokast.
   if (!strcmp(node->name, "UpperIdent")) {
     char *name = (char *)node->extra;
-    if (!validateIsTokName(tokast, name))
+    int tokfound = 0;
+    for (size_t i = 0; i < tokast->num_children; i++) {
+      char *tokname = (char *)tokast->children[i]->children[0]->extra;
+      if (!strcmp(name, tokname))
+        tokfound = 1;
+    }
+    if (!tokfound)
       ERROR("%s appears in the parser, but does not have a token definition.",
             name);
   }
 
+  // Ensure all rules are defined.
+  if (!strcmp(node->name, "LowerIdent")) {
+    char *name = (char *)node->extra;
+    int rulefound = 0;
+    for (size_t i = 0; i < names->len; i++) {
+      if (!strcmp(name, names->buf[i])) {
+        rulefound = 1;
+      }
+    }
+    if (!rulefound)
+      ERROR("%s appears as a rule, but has no definition.", name);
+  }
+
+  // Recurse to everything but labels.
   for (size_t i = 0; i < node->num_children; i++) {
-    validatePegVisit(node->children[i], tokast);
+    if (!strcmp(node->name, "Directive")) { // nop
+    } else if (!strcmp(node->name, "ModExpr")) {
+      validatePegVisit(node->children[0], tokast, names);
+      if (node->num_children > 1)
+        validateVisitLabel(node->children[1], names);
+    } else {
+      for (size_t i = 0; i < node->num_children; i++)
+        validatePegVisit(node->children[i], tokast, names);
+    }
   }
 }
 
 static inline void validatePegast(ASTNode *pegast, ASTNode *tokast) {
   if (!pegast)
     return;
-  resolvePrevNext(pegast);
-  validatePegVisit(pegast, tokast);
+
+  list_charptr names = resolvePrevNext(pegast);
+  validatePegVisit(pegast, tokast, &names);
+  list_charptr_clear(&names);
 }
 
 #endif /* PGEN_ASTVALID_INCLUDE */
